b1c134427e8feada3c66d2eab478a11f9401cbcd
Giulia Savorgnan <gsavorgnan@gsavorgnan-mac.ict.swin.edu.au>
2015-04-24 16:39:14 +1000

    friiiiiiday

--- python_codes/paper_figures/MbhMsph/figures.py
+++ python_codes/paper_figures/MbhMsph/figures.py
@@ -664,6 +664,219 @@ def mbh_vs_mass_sph_agn():
         plt.show()
 	#plt.savefig(path_paper_figures + 'mbh_vs_mass_sph_agn.pdf', format='pdf', dpi=1000)
 
+def mbh_vs_mass_sph():
+	
+	#outliers = [u'n1374', u'n3842exp', u'n4889']
+	outliers = []
+	
+	connection = sql3.connect(dbname)
+	cur = connection.cursor()
+
+	getdata_query = 'SELECT anc.gal_id, anc.mass_BH, anc.perr_mass_BH, anc.merr_mass_BH, anc.core, \
+		pysres.mag_sph_eq_moffat_comb, \
+		errV.perr_mag_sph, errV.merr_mag_sph, \
+		anc.ELLIPTICAL_my, anc.simplemorphtype, \
+		pysres.log_n_eq_moffat_comb, \
+		col.color  \
+		FROM Ancillary AS anc \
+		JOIN OneDFitResultsPhysicalUnits AS pysres ON anc.gal_id = pysres.gal_id \
+		JOIN ErrorsVote as errV ON anc.gal_id = errV.gal_id \
+		JOIN Colors as col ON anc.gal_id = col.gal_id \
+		WHERE anc.fit1D_done = 1;'
+	cur.execute(getdata_query)
+	datalist = cur.fetchall()
+	data= np.asarray(datalist).transpose()
+	#print data
+	gal_id = data[0]
+	mbh = data[1].astype(np.float)
+	log_mbh = np.log10(mbh)
+	perr_mbh = data[2].astype(np.float)
+	perr_log_mbh = np.log10(1 + perr_mbh/mbh)
+	merr_mbh = data[3].astype(np.float)
+	merr_log_mbh = -np.log10(1 - merr_mbh/mbh)
+	core = data[4].astype(np.int)
+	mag_sph = data[5].astype(np.float)
+	perr_mag_sph = data[6].astype(np.float)
+	merr_mag_sph = data[7].astype(np.float)
+	ELL = data[8].astype(np.int)
+	simplemorphtype = data[9]
+	log_n = data[10].astype(np.float)
+	n = 10**log_n
+	color = data[11].astype(np.float)
+	
+	log_ML = 3.98*color+0.13 # meidt+2014
+	ML = 10**log_ML
+	
+	mass_sph = ML*10**(-0.4*(mag_sph-3.25))
+	perr_mass_sph = ML*10**(-0.4*(mag_sph+perr_mag_sph-3.25)) - mass_sph
+	merr_mass_sph = -ML*10**(-0.4*(mag_sph-merr_mag_sph-3.25)) + mass_sph
+	
+	log_mass_sph = np.log10(mass_sph)
+	perr_log_mass_sph = np.log10(1+perr_mass_sph/mass_sph)
+	merr_log_mass_sph = -np.log10(1-merr_mass_sph/mass_sph)
+		
+        ELL_core = ELL*[0]
+        ELL_sersic = ELL*[0]
+        BUL_core = ELL*[0]
+        BUL_sersic = ELL*[0]
+       
+        earlytype = ELL*[0]
+	earlytype[simplemorphtype=='E'] = 1
+	earlytype[simplemorphtype=='S0'] = 1
+	earlytype[simplemorphtype=='E/S0'] = 1
+	
+	bulge = core*[0]
+	bulge[simplemorphtype=='S0'] = 1
+	bulge[simplemorphtype=='S'] = 1
+	bulge[simplemorphtype=='S0/S'] = 1
+	
+        for i in range(len(ELL)):
+        	if simplemorphtype[i]=='E' and core[i]==1 and gal_id[i] not in outliers:
+        		ELL_core[i] = 1
+        	elif simplemorphtype[i]=='E' and core[i]==0 and gal_id[i] not in outliers:
+        		ELL_sersic[i] = 1
+        	elif bulge[i]==1 and core[i]==1 and gal_id[i] not in outliers:
+        		BUL_core[i] = 1
+        	elif bulge[i]==1 and core[i]==0 and gal_id[i] not in outliers:
+        		BUL_sersic[i] = 1
+        
+	morph_coreList = []
+        for i in range(len(simplemorphtype)):
+        	if gal_id[i] not in outliers:
+			morph_coreList.append(simplemorphtype[i] + '_' + str(core[i]))
+		else:
+			morph_coreList.append('outlier')	
+        morph_core = np.asarray(morph_coreList)
+	       
+        fig, ax = plt.subplots()
+
+	scatter_kwargs = {"zorder":100}
+	error_kwargs = {"lw":.5, "zorder":0}
+
+	for x0,y0 in zip(mass_sph[simplemorphtype=='E'], mbh[simplemorphtype=='E']):
+		markers.elliptical(ax, 'red', np.log10(x0), np.log10(y0), 0.08)
+	
+ 	for x0,y0 in zip(mass_sph[simplemorphtype=='E/S0'], mbh[simplemorphtype=='E/S0']):
+		markers.lenticular(ax, 'darkorange', np.log10(x0), np.log10(y0), 0.08)
+       
+	for x0,y0 in zip(mass_sph[simplemorphtype=='S0'], mbh[simplemorphtype=='S0']):
+		markers.lenticular(ax, 'darkorange', np.log10(x0), np.log10(y0), 0.08)
+	
+        for x0,y0 in zip(mass_sph[simplemorphtype=='S0/Sp'], mbh[simplemorphtype=='S0/Sp']):
+		markers.spiral(ax, 'blue', np.log10(x0), np.log10(y0), 0.04)
+			
+        for x0,y0 in zip(mass_sph[simplemorphtype=='Sp'], mbh[simplemorphtype=='Sp']):
+		markers.spiral(ax, 'blue', np.log10(x0), np.log10(y0), 0.04)
+		
+	ax.scatter(mass_sph[simplemorphtype=='merger'], mbh[simplemorphtype=='merger'], marker=r'$\star$', s=500, color='k', **scatter_kwargs)	
+			
+	
+	print 'early'
+	print 'n', len(log_mass_sph[earlytype==1])
+        A,B,Aerr,Berr,covAB=bces.bces(log_mass_sph[earlytype==1]-np.average(log_mass_sph[earlytype==1]),
+		0.5*(perr_log_mass_sph[earlytype==1] + merr_log_mass_sph[earlytype==1]),
+        	log_mbh[earlytype==1],0.5*(merr_log_mbh[earlytype==1] + perr_log_mbh[earlytype==1]),log_mass_sph[earlytype==1]*[0.0])
+        print '---------------------------------'
+        print 'y = A*(x-<x>) + B '
+        print '<x> =', np.average(log_mass_sph[earlytype==1])
+        print
+        #print 'OLS(Y|X)    A =', "{0:.4f}".format(A[0]), '+-', "{0:.4f}".format(Aerr[0]), '   B = ', "{0:.4f}".format(B[0]), '+-', "{0:.4f}".format(Berr[0])
+        #print 'OLS(X|Y)    A =', "{0:.4f}".format(A[1]), '+-', "{0:.4f}".format(Aerr[1]), '   B = ', "{0:.4f}".format(B[1]), '+-', "{0:.4f}".format(Berr[1])
+        print 'bisector    A =', "{0:.4f}".format(A[2]), '+-', "{0:.4f}".format(Aerr[2]), '   B = ', "{0:.4f}".format(B[2]), '+-', "{0:.4f}".format(Berr[2])
+        #print 'orthogonal  A =', "{0:.4f}".format(A[3]), '+-', "{0:.4f}".format(Aerr[3]), '   B = ', "{0:.4f}".format(B[3]), '+-', "{0:.4f}".format(Berr[3])
+        print '---------------------------------'
+       
+        logxx = np.arange(-10,20,0.1)
+        yy = (A[2]*(logxx) + B[2])
+        ax.plot(10**(logxx+np.average(log_mass_sph[earlytype==1])),10**yy, color='r', ls='--', linewidth=2.)
+	#colorline.colorline(10**(logxx+np.average(log_mass_sph[earlytype==1])), 10**yy, cmap=green_red)
+	
+	##### calculates 1sigma uncertainty band
+	yy_1 = ((A[2]+Aerr[2])*(logxx) + (B[2]+Berr[2]))
+	yy_2 = ((A[2]-Aerr[2])*(logxx) + (B[2]+Berr[2]))
+	yy_3 = ((A[2]+Aerr[2])*(logxx) + (B[2]-Berr[2]))
+	yy_4 = ((A[2]-Aerr[2])*(logxx) + (B[2]-Berr[2]))
+	yy_up = yy_1*[0.0]
+	for i in range(len(yy_1)):
+		if yy_1[i] > yy_2[i]:
+			yy_up[i] = yy_1[i]
+		elif yy_1[i] <= yy_2[i]:
+			yy_up[i] = yy_2[i]	
+	yy_up = np.asarray(yy_up)
+	yy_lo = yy_1*[0.0]
+	for i in range(len(yy_3)):
+		if yy_3[i] < yy_4[i]:
+			yy_lo[i] = yy_3[i]
+		elif yy_3[i] >= yy_4[i]:
+			yy_lo[i] = yy_4[i]	
+	yy_lo = np.asarray(yy_lo)
+				
+	ax.fill_between(10**(logxx+np.average(log_mass_sph[earlytype==1])), 10**yy_lo, 10**yy_up, alpha=0.1, facecolor='r')
+	
+        print 'sersic bul of spi'
+        print 'n', len(log_mass_sph[morph_core=='Sp_0'])
+        A,B,Aerr,Berr,covAB=bces.bces(log_mass_sph[morph_core=='Sp_0']-np.average(log_mass_sph[morph_core=='Sp_0']),
+		0.5*(perr_log_mass_sph[morph_core=='Sp_0'] + merr_log_mass_sph[morph_core=='Sp_0']),
+        	log_mbh[morph_core=='Sp_0'],0.5*(merr_log_mbh[morph_core=='Sp_0'] + perr_log_mbh[morph_core=='Sp_0']),log_mass_sph[morph_core=='Sp_0']*[0.0])
+        print '---------------------------------'
+        print 'y = A*(x-<x>) + B '
+        print '<x> =', np.average(log_mass_sph[morph_core=='Sp_0'])
+        print
+        #print 'OLS(Y|X)    A =', "{0:.4f}".format(A[0]), '+-', "{0:.4f}".format(Aerr[0]), '   B = ', "{0:.4f}".format(B[0]), '+-', "{0:.4f}".format(Berr[0])
+        #print 'OLS(X|Y)    A =', "{0:.4f}".format(A[1]), '+-', "{0:.4f}".format(Aerr[1]), '   B = ', "{0:.4f}".format(B[1]), '+-', "{0:.4f}".format(Berr[1])
+        print 'bisector    A =', "{0:.4f}".format(A[2]), '+-', "{0:.4f}".format(Aerr[2]), '   B = ', "{0:.4f}".format(B[2]), '+-', "{0:.4f}".format(Berr[2])
+        #print 'orthogonal  A =', "{0:.4f}".format(A[3]), '+-', "{0:.4f}".format(Aerr[3]), '   B = ', "{0:.4f}".format(B[3]), '+-', "{0:.4f}".format(Berr[3])
+        print '---------------------------------'
+       
+        logxx = np.arange(-10,20,0.1)
+        yy = (A[2]*(logxx) + B[2])
+        ax.plot(10**(logxx+np.average(log_mass_sph[morph_core=='Sp_0'])),10**yy, color='b', ls='-', linewidth=2.)
+	
+	##### calculates 1sigma uncertainty band
+	yy_1 = ((A[2]+Aerr[2])*(logxx) + (B[2]+Berr[2]))
+	yy_2 = ((A[2]-Aerr[2])*(logxx) + (B[2]+Berr[2]))
+	yy_3 = ((A[2]+Aerr[2])*(logxx) + (B[2]-Berr[2]))
+	yy_4 = ((A[2]-Aerr[2])*(logxx) + (B[2]-Berr[2]))
+	yy_up = yy_1*[0.0]
+	for i in range(len(yy_1)):
+		if yy_1[i] > yy_2[i]:
+			yy_up[i] = yy_1[i]
+		elif yy_1[i] <= yy_2[i]:
+			yy_up[i] = yy_2[i]	
+	yy_up = np.asarray(yy_up)
+	yy_lo = yy_1*[0.0]
+	for i in range(len(yy_3)):
+		if yy_3[i] < yy_4[i]:
+			yy_lo[i] = yy_3[i]
+		elif yy_3[i] >= yy_4[i]:
+			yy_lo[i] = yy_4[i]	
+	yy_lo = np.asarray(yy_lo)
+				
+	ax.fill_between(10**(logxx+np.average(log_mass_sph[morph_core=='Sp_0'])), 10**yy_lo, 10**yy_up, alpha=0.1, facecolor='b')
+	
+	
+       #### fit using FITEXY ###
+       #print 'sersic bul'
+       #A,perr_A,merr_A,B,perr_B,merr_B = fitexy.bisect_modfitexy(log_mass_sph[BUL_sersic==1]-np.average(log_mass_sph[BUL_sersic==1]),
+       #	0.5*(perr_log_mass_sph[BUL_sersic==1] + merr_log_mass_sph[BUL_sersic==1]),
+       #	log_mbh[BUL_sersic==1],0.5*(merr_log_mbh[BUL_sersic==1] + perr_log_mbh[BUL_sersic==1]))
+       #logxx = np.arange(-10,20,0.1)
+       ## plot bisector relation
+       #y_bisec = A + B*logxx
+       #ax.plot(10**(logxx+np.average(log_mass_sph[BUL_sersic==1])), 10**y_bisec, ls='-.', color='blue', linewidth=2.)
+       ##label_ell = r'$B_{\rm ell} = ' + str("{0:.2f}".format(B)) + r'^{+' + str("{0:.2f}".format(perr_B)) + r'}_{-' + str("{0:.2f}".format(merr_B)) +r'}$'
+       ##ax.text(-24, 10**6.7, label_ell, fontsize=20)
+	
+	ax.set_xscale('log')
+	ax.set_yscale('log')
+        plt.axis([10**8.6,10**12.3,10**5.3,10**11.2])
+        plt.xlabel(r'$M_{\rm *,sph}\rm~[M_\odot]$', labelpad=15)
+        plt.ylabel(r'$M_{\rm BH} \rm ~[M_\odot]$', labelpad=15)
+	plt.subplots_adjust(left=0.15,bottom=0.15,right=0.97,top=0.9)
+        #plt.show()
+	plt.savefig(path_paper_figures + 'mbh_vs_mass_sph.pdf', format='pdf', dpi=1000)
+
+
 def mbh_vs_mag_sph_psb():
 
 	#outliers = [u'n1374', u'n3842exp', u'n4889']
@@ -2021,7 +2234,8 @@ def mbh_vs_mass_sph_galsymb_agn():
 		
 def main():
 	#mag_lit_vs_mag_my()
-	mbh_vs_mass_sph_agn()
+	#mbh_vs_mass_sph_agn()
+	mbh_vs_mass_sph()
 	#mbh_vs_mag_sph_psb()
 	#mbh_vs_mag_tot()
 	#mbh_vs_mag_sph()
